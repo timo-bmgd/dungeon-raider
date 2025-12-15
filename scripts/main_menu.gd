2e380903f5c533b5bcb747c1ed7697d38ab4bc68
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2025-12-15 17:00:06 +0100

    cleanup

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,20 +1,11 @@
 extends Control
 
-enum Status {MAIN, INGAME}
-
-var status = Status.MAIN
-
-var progress = []
-var sceneName
-var scene_load_status = 0
 var scenes_path = "res://scenes/%s.tscn"
-
 
 @onready var loading_status: Label = $"../LoadingStatus"
 
 	
 func _on_start_game_pressed() -> void:
-	status = Status.INGAME
 	var scene_name = scenes_path % "game"
 	SceneManager.goto_scene(scene_name)
 	# load_scene("game")
@@ -26,6 +17,5 @@ func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_back_to_menu_pressed() -> void:
-	status = Status.MAIN
 	var scene_name = scenes_path % "main_menu"
 	SceneManager.goto_scene(scene_name)
