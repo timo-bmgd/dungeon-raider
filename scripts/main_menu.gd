bf2dae37e60bbf10cbd45d46d1a30b441aaf4a51
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2025-12-15 18:15:31 +0100

    fully refactor scene loading incl. hiding of buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,26 @@
 extends Control
 
-var scenes_path = "res://scenes/%s.tscn"
 
 @onready var loading_status: Label = $"../LoadingStatus"
 
+@onready var start_game: Button = $StartGame
+@onready var back_to_menu: Button = $BackToMenu
+@onready var options: Button = $Options
+@onready var exit: Button = $Exit
+
+func _ready():
+	print("asking for update to STATE")
+	match SceneManager.state:
+		SceneManager.State.GAME:
+			start_game.visible = false;
+			back_to_menu.visible = true;
+		SceneManager.State.MAIN_MENU:
+			start_game.visible = true;
+			back_to_menu.visible = false;
+
 	
 func _on_start_game_pressed() -> void:
-	var scene_name = scenes_path % "game"
-	SceneManager.goto_scene(scene_name)
+	SceneManager.goto_scene("game")
 	# load_scene("game")
 
 func _on_options_pressed() -> void:
@@ -17,5 +30,4 @@ func _on_exit_pressed() -> void:
 	get_tree().quit()
 
 func _on_back_to_menu_pressed() -> void:
-	var scene_name = scenes_path % "main_menu"
-	SceneManager.goto_scene(scene_name)
+	SceneManager.goto_scene("main_menu")
--- scripts/scene_manager.gd
+++ scripts/scene_manager.gd
@@ -1,13 +1,17 @@
 extends Node
 
 var current_scene = null
+var scenes_path = "res://scenes/%s.tscn"
+
+enum State {MAIN_MENU, GAME}
+var state = State.MAIN_MENU
 
 func _ready():
 	var root = get_tree().root
 	# Using a negative index counts from the end, so this gets the last child node of `root`.
 	current_scene = root.get_child(-1)
 	
-func goto_scene(path):
+func goto_scene(scene_name):
 	# This function will usually be called from a signal callback,
 	# or some other function in the current scene.
 	# Deleting the current scene at this point is
@@ -16,14 +20,24 @@ func goto_scene(path):
 
 	# The solution is to defer the load to a later time, when
 	# we can be sure that no code from the current scene is running:
+	match scene_name:
+		"game":
+			state = State.GAME
+		"main_menu":
+			state = State.MAIN_MENU
+		_:
+			state = State.MAIN_MENU
 
-	_deferred_goto_scene.call_deferred(path)
+	_deferred_goto_scene.call_deferred(scene_name)
 
 
-func _deferred_goto_scene(path):
+func _deferred_goto_scene(scene_name):
 	# It is now safe to remove the current scene.
 	current_scene.free()
-
+	
+	# Construct path
+	var path = scenes_path % scene_name
+	
 	# Load the new scene.
 	var s = ResourceLoader.load(path)
 	
@@ -35,3 +49,7 @@ func _deferred_goto_scene(path):
 
 	# Optionally, to make it compatible with the SceneTree.change_scene_to_file() API.
 	get_tree().current_scene = current_scene
+	
+	print("updating STATE to %s" % scene_name)
+	
+		
