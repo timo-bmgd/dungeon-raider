126b4dcb72eea969c98420ceeec0c35250db91c1
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2025-12-15 19:42:15 +0100

    add tilemap loading in game manager

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,6 +1,13 @@
 extends Node
 
+enum Level {SPACESHIP, DUNGEON}
+var current_level = Level.SPACESHIP
+
+var scenes_path = "res://scenes/%s.tscn"
+
 @onready var ui: CanvasLayer = %ui
+@onready var level_container: Node2D = %LevelContainer
+
 
 var inventory = Array()
 
@@ -8,3 +15,18 @@ func collect_item(item):
 	inventory.append(item)
 	print(item)
 	ui.add_item(item)
+
+func _ready() -> void:
+	# Set the SPACESHIP as the level
+	load_level("level_dungeon")
+	
+
+func clear_levels():
+	for child in level_container.get_children():
+		child.queue_free()
+		
+		
+func load_level(scene_name):
+	clear_levels()
+	var level: Resource = ResourceLoader.load(scenes_path % scene_name)
+	level_container.add_child(level.instantiate())
