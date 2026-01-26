3fd503110a355f0dd671b2b9636692a4e018cb1c
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2026-01-26 15:41:46 +0100

    cleanup

--- scripts/lever.gd
+++ scripts/lever.gd
@@ -6,15 +6,11 @@ var activated := 0
 var selected := 0
 
 func _on_lever_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	# Alternative: if you really want to use your "click" action
 	if event.is_action_pressed("click"):
 		activated = int(!bool(activated)) # genius
-		print("Lever clicked:" + str(activated))
 		set_animation_frame()
 
 func set_animation_frame() -> void:
-	print("activated:"+str(activated))
-	print("selected:"+str(selected))
 	animated_sprite_2d.frame = activated + selected
 	
 
