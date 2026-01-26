4e6467ba9236c05f5dd8d5bf311043c93c39648d
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2026-01-26 15:12:42 +0100

    WIP lever

--- scenes/lever.gd
+++ scenes/lever.gd
@@ -1,20 +1,16 @@
 extends AnimatedSprite2D
-			
-			
-func _ready() -> void:
-	print("sready")
-	
+
+var activated := false
 	
 func _on_lever_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	# Alternative: if you really want to use your "click" action
 	if event.is_action_pressed("click"):
-		print("Sprite clicked via action!")
-
+		activated = !activated
+		print("Lever clicked:" + str(activated))
 
 func _on_lever_area_mouse_entered() -> void:
 	print("mouse entered")
 	pass # Replace with function body.
-
 
 func _on_lever_area_mouse_exited() -> void:
 	print("mouse left")
