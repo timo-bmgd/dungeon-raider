dcd64970e22ae99fa73e4456f93fadee22419b3e
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2026-01-26 18:38:44 +0100

    slight adaptations to level, make inv scrollable

--- scripts/sell_zone.gd
+++ scripts/sell_zone.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 @onready var area_2d: Area2D = $Area2D
-@onready var score: Label = $Score
+@onready var credit_label: Label = $credit_label
+@onready var items_label: Label = $items_label
+
+
 var current_money = 0
+var current_items = 0
 
 func _on_lever_lever_activated(is_on: bool) -> void:
 	if(is_on):
@@ -18,4 +22,16 @@ func _on_lever_lever_activated(is_on: bool) -> void:
 		current_money += money_made
 		for item in items:
 			item.queue_free()
-		score.text = "Current Money: " + str(current_money)
+		credit_label.text = "Credit: " + str(current_money)
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is Item:
+		current_items += 1
+		items_label.text = "Items: " + str(current_items)
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body is Item:
+		current_items -= 1
+		items_label.text = "Items: " + str(current_items)
