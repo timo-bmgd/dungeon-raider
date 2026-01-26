68c0517f7d5f138f6309fc2ba3aea6aadbe897fa
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2026-01-26 15:50:44 +0100

    add lever signal, to be able to reuse lever scene

--- scripts/lever.gd
+++ scripts/lever.gd
@@ -2,13 +2,16 @@ extends Node2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2d
 
+signal lever_activated(is_on: bool)
+
 var activated := 0
 var selected := 0
 
-func _on_lever_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_lever_area_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
 		activated = int(!bool(activated)) # genius
 		set_animation_frame()
+		lever_activated.emit(bool(activated))
 
 func set_animation_frame() -> void:
 	animated_sprite_2d.frame = activated + selected
