5d5859bc7cdb2986269e1b8e1b2534a1e9e10379
Timo Boomgaarden <78025568+timo-bmgd@users.noreply.github.com>
2026-01-26 15:37:44 +0100

    make lever nicely selectable

--- scenes/lever.gd
+++ scenes/lever.gd
@@ -1,17 +1,28 @@
-extends AnimatedSprite2D
+extends Node2D
 
-var activated := false
-	
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2d
+
+var activated := 0
+var selected := 0
+
 func _on_lever_area_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	# Alternative: if you really want to use your "click" action
 	if event.is_action_pressed("click"):
-		activated = !activated
+		activated = int(!bool(activated)) # genius
 		print("Lever clicked:" + str(activated))
+		set_animation_frame()
+
+func set_animation_frame() -> void:
+	print("activated:"+str(activated))
+	print("selected:"+str(selected))
+	animated_sprite_2d.frame = activated + selected
+	
 
 func _on_lever_area_mouse_entered() -> void:
-	print("mouse entered")
-	pass # Replace with function body.
+	selected = int(!bool(selected))*2 # genius
+	set_animation_frame()
+
 
 func _on_lever_area_mouse_exited() -> void:
-	print("mouse left")
-	pass # Replace with function body.
+	selected = int(!bool(selected))*2 # genius
+	set_animation_frame()
